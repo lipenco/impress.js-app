42502d09faa578bd02afda51a4c439e286697613
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-22 13:02:42 +0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImpressJsApp::Application.config.secret_token = 'af4069e2294279a36c7160790106310fa2f0e2e61e18342b326b8011176d2a3d4e44d9a732d81390b4b75447afb0833331e3a53a49cc69a3f7ec5f7c7f4a5f1b'
