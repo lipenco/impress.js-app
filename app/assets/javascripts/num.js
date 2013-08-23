fc554f256b117864a6a51a3042ff7f3e00e0e429
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-23 14:44:25 +0400

    adding event listener for input

--- app/assets/javascripts/num.js
+++ app/assets/javascripts/num.js
@@ -1,2 +1,6 @@
+
+var getNumberFromForm = function () {
 num_slides = document.getElementById("num_slides").value;
 document.getElementById("preview").src = "presentation?num_slides=" + num_slides;
+}
+document.addEventListener("keyup", getNumberFromForm, false);
--- app/controllers/presentation_controller.rb
+++ app/controllers/presentation_controller.rb
@@ -1,6 +1,6 @@
 class PresentationController < ApplicationController
   def build
-    @num_slides = 2
+    @num_slides = 0
     @num_slides = params[:num_slides].to_i if params[:num_slides].to_i > 0
   end
 
