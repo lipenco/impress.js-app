ec6821faa7a3010e7cf95cc4bc26b3ebcc3262a0
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-25 16:58:59 +0400

    move impress to public

--- app/assets/javascripts/num.js
+++
@@ -1,6 +0,0 @@
-
-var getNumberFromForm = function () {
-num_slides = document.getElementById("num_slides").value;
-document.getElementById("preview").src = "presentation?num_slides=" + num_slides;
-}
-document.addEventListener("keyup", getNumberFromForm, false);
