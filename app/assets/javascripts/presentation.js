36631e7625d94bdd088448f31829647c6c9ee29a
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-26 16:15:59 +0400

    adding layout part1

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -1,6 +1,27 @@
-var getNumberFromForm = function() {
+var updatePreview = function() {
   num_slides = document.getElementById("num_slides").value;
-  document.getElementById("preview").src = "presentation?num_slides=" + num_slides;
+  layout = getLayout();
+  document.getElementById("preview").src = "presentation?num_slides=" + num_slides + "&layout=" + layout;
 }
 
-document.addEventListener("keyup", getNumberFromForm, false);
+var getLayout = function() {
+  var layoutType = "vertical";
+  if(document.querySelector('#vertical.active')) {
+    layoutType = document.getElementById("vertical").value;
+  }
+  else if(document.querySelector('#linear.active')) {
+    layoutType = document.getElementById("linear").value;
+  }
+  else if (document.querySelector('#circle.active')) {
+    layoutType = document.getElementById("circle").value;
+  }
+  return layoutType;
+}
+
+document.addEventListener('keyup', updatePreview, false);
+
+var buttons = document.querySelectorAll(".btn");
+for (var i = 0; i<buttons.length; i++) {
+  console.log(buttons[i]);
+  buttons[i].addEventListener('click', updatePreview, false);
+}
--- app/controllers/presentation_controller.rb
+++ app/controllers/presentation_controller.rb
@@ -2,6 +2,8 @@ class PresentationController < ApplicationController
   def build
     @num_slides = 0
     @num_slides = params[:num_slides].to_i if params[:num_slides].to_i > 0
+    @layout = "vertical"
+    @layout = params[:layout] if ["vertical", "linear", "circle"].include?(params[:layout])
   end
 
   def home
