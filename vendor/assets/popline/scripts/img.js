04511ad513ed783f3c99b5b4166585f2308f7b8c
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-10-05 18:13:48 +0900

    adding upload option to images popline

--- vendor/assets/popline/scripts/img.js
+++ vendor/assets/popline/scripts/img.js
@@ -2,10 +2,15 @@
 
 
   var buildTextField = function(popline, button) {
+
     if (button.find(":text").length === 0) {
       var $textField = $("<input type='text' />");
       $textField.addClass("textfield");
       $textField.attr("placeholder", "Type Url Here");
+
+      var $buttonField = $("<div class='popline-picture'><i class='icon-upload'></i> Upload</div>");
+      $buttonField.addClass("add-picture");
+
 
       $textField.keyup(function(event) {
         if (event.which === 13) {
@@ -21,6 +26,7 @@
         event.stopPropagation();
       });
       button.append($textField);
+      button.append($buttonField);
     }
   }
 
@@ -31,15 +37,18 @@
       iconClass: "icon-picture",
       mode: "edit",
       beforeShow: function(popline) {
+        
      
-
         if (!this.data("click-event-binded")) {
           
           this.click(function(event) {
+
           var $_this = $(this);
           buildTextField(popline, $_this);
 
+
         if (!$_this.hasClass("boxed")) {
+
             popline.switchBar($_this, function() {
             $_this.siblings("li").hide().end()
                     .children(":text").show().end()
@@ -48,9 +57,11 @@
                 });
                 $_this.data('selection', window.getSelection().getRangeAt(0));
                 event.stopPropagation();
+
               }  
           });
           this.data("click-event-binded", true);
+
         }
       },
       afterHide: function() {
