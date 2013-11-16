2b5eadd405c90bda46f2d3ec1930e54c7f221006
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-11-16 19:47:23 +0900

    fixing routing bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to "/home", :notice => "Signed up!"
+      redirect_to "/build", :notice => "Signed up!"
     else
       render "new"
     end
