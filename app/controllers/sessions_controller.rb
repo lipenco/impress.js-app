b7996253ef9f5cbd754878340a2bbf10500a5aca
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-11-25 15:34:37 +0900

    uthenticate test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,34 +2,16 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      if params[:remember_me]
-        cookies.permanent[:auth_token] = user.auth_token
-      else
-        cookies[:auth_token] = user.auth_token
-      end
-      redirect_to "/build", :notice => "Logged in!"
-      # redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    # cookies.delete(:auth_token)
-    redirect_to "/build", :notice => "Logged out!"
-    # redirect_to root_url, :notice => "Logged out!"
-  end
-
   # def create
   #   user = User.find_by_email(params[:email])
   #   if user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
+  #     if params[:remember_me]
+  #       cookies.permanent[:auth_token] = user.auth_token
+  #     else
+  #       cookies[:auth_token] = user.auth_token
+  #     end
   #     redirect_to "/build", :notice => "Logged in!"
+  #     # redirect_to root_url, :notice => "Logged in!"
   #   else
   #     flash.now.alert = "Invalid email or password"
   #     render "new"
@@ -38,6 +20,24 @@ class SessionsController < ApplicationController
 
   # def destroy
   #   session[:user_id] = nil
+  #   # cookies.delete(:auth_token)
   #   redirect_to "/build", :notice => "Logged out!"
+  #   # redirect_to root_url, :notice => "Logged out!"
   # end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/build", :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to "/build", :notice => "Logged out!"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = @user.id
+      # session[:user_id] = @user.id
       redirect_to "/build", :notice => "Signed up!"
     else
       render "new"
